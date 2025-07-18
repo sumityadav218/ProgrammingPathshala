a7fd885260e1972818853b0a0904513857d983c0
Sumit Kumar Yadav <syadav@corelogic.com>
2025-07-18 14:06:14 +0530

    Initial commit

---
+++ src/Arrays/Precomputation/Question1.java
@@ -0,0 +1,25 @@
+package Arrays.Precomputation;
+
+public class Question1 {
+
+    public static int[] precompute(int[] arr) {
+        int n = arr.length;
+        int[] precomputed = new int[n];
+        precomputed[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            precomputed[i] = precomputed[i - 1] + arr[i];
+        }
+
+        return precomputed;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] result = precompute(arr);
+
+        for (int value : result) {
+            System.out.print(value + " ");
+        }
+    }
+}
---
+++ src/Arrays/Precomputation/Question2.java
@@ -0,0 +1,46 @@
+package Arrays.Precomputation;
+
+
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        //taking inputs from the console
+        System.out.println("enter the size of array:");
+        int n = sc.nextInt() ;
+        System.out.println("enter value of p:");
+        long p =sc.nextLong() ;
+        System.out.println("enter value of q");
+        long q = sc.nextLong() ;
+        System.out.println("enter value of r:");
+        long r = sc.nextLong()  ;
+        int[] arr =  new int[n] ;
+        for (int i = 0; i < n; i++) {
+            System.out.println("enter the input array:");
+            arr[i] = sc.nextInt() ;
+        }
+
+        //precomputation of prefix sum array
+        long[] prefixSum = new long [n] ;
+        prefixSum[0] = p*arr[0] ;
+        for (int i = 1 ; i< n ;i++){
+            prefixSum[i]= Math.max(prefixSum[i-1],p*arr[i]) ;
+        }
+
+        //precomputation of suffix sum arrray
+        long[] suffixSum =  new long[n] ;
+        suffixSum[n-1] = r*arr[n-1] ;
+        for (int i = n-2 ; i>=0 ;i--){
+            suffixSum[i]= Math.max(suffixSum[i+1],r*arr[i]) ;
+        }
+
+        //final step to calculate the maximum value
+        long result = Long.MIN_VALUE ;
+        System.out.println("Maximum result is : ");
+        for (int i = 0 ; i < n ;i++){
+            result = Math.max(result, prefixSum[i] + q * arr[i] + suffixSum[i]) ;
+        }
+        System.out.println(result);
+    }
+}
